94dbd92f0b15feae67622bb4fe136a14fb69791a
EMRAZ1 <egokamix56@gmail.com>
2024-02-21 14:12:18 +0200

    Зробив пасажирський потяг

--- LabaNomer2/PassengerCarriage.cs
+++ LabaNomer2/PassengerCarriage.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace LabaNomer2
 {
-    internal class PassengerCarriage
+    public class PassengerCarriage : Carriage
     {
+        public int SeatsCount { get; set; }
+        public int Passengers { get; set; }
+
+        public PassengerCarriage(string id, double weight, double length, double loadCapacity, int seatsCount)
+            : base(id, "Passenger", weight, length, loadCapacity)
+        {
+            SeatsCount = seatsCount;
+        }
+
+        public void LoadPassengers()
+        {
+            Console.WriteLine("Введіть кількість пасажирів:");
+            Passengers = Convert.ToInt32(Console.ReadLine());
+            if (Passengers > SeatsCount)
+            {
+                Console.WriteLine("Кількість пасажирів не може перевищувати кількість місць.");
+                Passengers = SeatsCount;
+            }
+        }
     }
 }
