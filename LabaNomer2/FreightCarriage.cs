663ba611e12ea846c35b44ed2b9bd095d7a099ee
EMRAZ1 <egokamix56@gmail.com>
2024-02-21 14:18:01 +0200

    Зробив клас вантажу

--- LabaNomer2/FreightCarriage.cs
+++ LabaNomer2/FreightCarriage.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace LabaNomer2
 {
-    internal class FreightCarriage
+    public class FreightCarriage : Carriage
     {
+        public double Load { get; set; }
+
+        public FreightCarriage(string id, double weight, double length, double loadCapacity)
+            : base(id, "Freight", weight, length, loadCapacity)
+        {
+            Load = 0;
+        }
+
+        public void LoadCargo()
+        {
+            Console.WriteLine("Введіть вагу вантажу:");
+            double cargoWeight = Convert.ToDouble(Console.ReadLine());
+            if (cargoWeight > LoadCapacity)
+            {
+                Console.WriteLine("Вантаж не може перевищувати максимальну вантажопідйомність вагона.");
+            }
+            else
+            {
+                Load = cargoWeight;
+            }
+        }
     }
 }
