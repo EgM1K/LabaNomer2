975105a7fc3ac36186bfe9eaf4877e80b55f163e
EMRAZ1 <egokamix56@gmail.com>
2024-02-21 14:22:36 +0200

    Зробив клас потягу

--- LabaNomer2/TrainDR1.cs
+++ LabaNomer2/TrainDR1.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace LabaNomer2
 {
-    internal class TrainDR1
+    public class Train
     {
+        public string Name { get; set; }
+        public double MaxWeight { get; set; }
+        public List<Carriage> Carriages { get; set; }
+
+        public Train(string name, double maxWeight)
+        {
+            Name = name;
+            MaxWeight = maxWeight;
+            Carriages = new List<Carriage>();
+        }
+
+        public void AddCarriage(Carriage carriage)
+        {
+            double currentWeight = Carriages.Sum(c => c.Weight + c.LoadCapacity);
+            if (currentWeight + carriage.Weight + carriage.LoadCapacity > MaxWeight)
+            {
+                Console.WriteLine("Не можна додати вагон, оскільки це перевищить максимальну вагу потягу.");
+            }
+            else
+            {
+                Carriages.Add(carriage);
+            }
+        }
     }
 }
