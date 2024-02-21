aafe450c5bfda1013fc4e56dcf58e3b248050dbe
EMRAZ1 <egokamix56@gmail.com>
2024-02-21 14:15:49 +0200

    Додав клас вантажного вагону

---
+++ LabaNomer2/FreightCarriage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaNomer2
+{
+    internal class FreightCarriage
+    {
+    }
+}
