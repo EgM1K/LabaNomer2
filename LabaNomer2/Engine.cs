57981639c07bf67d3133b9bdd2382e5172f4fa89
EMRAZ1 <egokamix56@gmail.com>
2024-02-21 15:22:28 +0200

    Почав робити клас рушія, зараз додам метод підрахування швидкості

--- LabaNomer2/Engine.cs
+++ LabaNomer2/Engine.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace LabaNomer2
 {
-    internal class Engine
+    public class Engine
     {
+        public string Name { get; set; }
+        public double MaxSpeed { get; set; }
+        public double MaxLoad { get; set; }
+
+        public Engine(string name, double maxSpeed, double maxLoad)
+        {
+            Name = name;
+            MaxSpeed = maxSpeed;
+            MaxLoad = maxLoad;
+        }
     }
 }
