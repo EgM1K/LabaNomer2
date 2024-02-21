472a9e99204252ca865163b758f9a654d9e4ecb5
EMRAZ1 <egokamix56@gmail.com>
2024-02-21 14:36:06 +0200

    Доповнив клас треін виподковою ситуацією та виводом інформації, та зробив інтерфейс

---
+++ LabaNomer2/ITrainDR1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaNomer2
+{
+    public interface ITrain
+    {
+        string Name { get; set; }
+        double MaxWeight { get; set; }
+        List<Carriage> Carriages { get; set; }
+
+        void AddCarriage(Carriage carriage);
+        void SimulateJourney();
+        void PrintTrainInfo();
+    }
+}
--- LabaNomer2/TrainDR1.cs
+++ LabaNomer2/TrainDR1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LabaNomer2
 {
@@ -29,6 +30,50 @@ namespace LabaNomer2
             else
             {
                 Carriages.Add(carriage);
+            }
+        }
+    }
+    public void SimulateJourney()
+    {
+        Random random = new Random();
+        double accidentChance = random.NextDouble();
+        if (accidentChance < 0.001)
+        {
+            Console.WriteLine("Сталася аварія! Потяг перекинувся.");
+        }
+        else
+        {
+            foreach (var carriage in Carriages)
+            {
+                if (carriage is PassengerCarriage passengerCarriage)
+                {
+                    double passengerAccidentChance = random.NextDouble();
+                    if (passengerAccidentChance < 0.02)
+                    {
+                        int affectedPassengers = random.Next(1, passengerCarriage.Passengers + 1);
+                        Console.WriteLine($"У {affectedPassengers} пасажирів сталися неприємності.");
+                        passengerCarriage.Passengers -= affectedPassengers;
+                    }
+                }
+            }
+        }
+    }
+    public void PrintTrainInfo()
+    {
+        Console.WriteLine($"Назва потягу: {Name}");
+        Console.WriteLine($"Максимальна вага: {MaxWeight}");
+        Console.WriteLine($"Кількість вагонів: {Carriages.Count}");
+        Console.WriteLine("Інформація про вагони:");
+        foreach (var carriage in Carriages)
+        {
+            Console.WriteLine($"ID: {carriage.Id}, Тип: {carriage.Type}, Вага: {carriage.Weight}, Довжина: {carriage.Length}");
+            if (carriage is PassengerCarriage passengerCarriage)
+            {
+                Console.WriteLine($"Кількість пасажирів: {passengerCarriage.Passengers}");
+            }
+            else if (carriage is FreightCarriage freightCarriage)
+            {
+                Console.WriteLine($"Вантаж: {freightCarriage.Load}");
             }
         }
     }
