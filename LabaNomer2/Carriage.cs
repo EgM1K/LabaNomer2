92200e9ad10e6d556d03276a8e098cf9f6cfb150
EMRAZ1 <egokamix56@gmail.com>
2024-02-21 14:08:28 +0200

    Зробив клас для вагонів

--- LabaNomer2/Carriage.cs
+++ LabaNomer2/Carriage.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace LabaNomer2
 {
-    internal class Carriage
+    public abstract class Carriage
     {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public double Weight { get; set; }
+        public double Length { get; set; }
+        public double LoadCapacity { get; set; }
+
+        protected Carriage(string id, string type, double weight, double length, double loadCapacity)
+        {
+            Id = id;
+            Type = type;
+            Weight = weight;
+            Length = length;
+            LoadCapacity = loadCapacity;
+        }
     }
 }
